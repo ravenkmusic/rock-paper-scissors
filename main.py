1f23143df654388b58cbbce7048985424347706c
Raven K <85319728+ravenkmusic@users.noreply.github.com>
2022-01-08 20:36:15 +0000

    added one more scenario to account for any user input outside of range given

--- main.py
+++ main.py
@@ -33,16 +33,19 @@ num_choices = len(choices)
 
 users_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-print(f"You chose: {choices[users_choice]}")
+if users_choice >= 3 or users_choice <0:
+  print("You have entered an invalid number. You lose.")
+else:
+  print(f"You chose: {choices[users_choice]}")
 
-computer_choice = random.randint(0, 2)
-print(f"Computer chose: {choices[computer_choice]}")
+  computer_choice = random.randint(0, 2)
+  print(f"Computer chose: {choices[computer_choice]}")
 
-if users_choice == computer_choice:
-  print("You both chose the same thing. It's a draw.")
-elif users_choice == 0 and computer_choice == 2:
-  print("You win.")
-elif computer_choice == 0 and users_choice == 2:
-  print("You lose.")
-elif computer_choice > users_choice:
-  print("You lose.")
+  if users_choice == 0 and computer_choice == 2:
+    print("You win.")
+  elif computer_choice == 0 and users_choice == 2:
+      print("You lose.")
+  elif computer_choice > users_choice:
+      print("You lose.")
+  elif users_choice == computer_choice:
+      print("You both chose the same thing. It's a draw.")
