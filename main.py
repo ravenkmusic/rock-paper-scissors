9ba982ffce3fb54be1d677a758308d7ce9afb0a7
Raven K <85319728+ravenkmusic@users.noreply.github.com>
2022-01-08 20:03:34 +0000

    added line break after prompt and before user input

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ import random
 choices = [rock, paper, scissors]
 num_choices = len(choices)
 
-users_choice = choices[int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))]
+users_choice = choices[int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))]
 
 computer_choice = choices[random.randint(0, num_choices - 1)]
 
@@ -39,16 +39,23 @@ computer_choice = choices[random.randint(0, num_choices - 1)]
 print(f"You chose: {users_choice}")
 print(f"Computer chose: {computer_choice}")
 
+if users_choice == 0 and computer_choice == 2:
+  print("You win.")
+
+if users_choice == 1 and computer_choice == 0:
+  print("You win.")
+
+if users_choice == 2 and computer_choice == 1:
+  print("You win.")
+
+if users_choice == 2 and computer_choice == 0:
+  print("You lose.")
+
+if users_choice == 0 and computer_choice == 1:
+  print("You lose.")
+
+if users_choice == 1 and computer_choice == 2:
+  print("You lose.")
+
 if users_choice == computer_choice:
   print(f"You both chose the same thing. It's a draw.")
-elif users_choice == 0 and computer_choice == 2:
-  print("You win.")
-
-elif users_choice == 1 and computer_choice == 0:
-  print("You win.")
-
-elif users_choice == 2 and computer_choice == 1:
-  print("You win.")
-
-else:
-  print("You lose.")
