71f282974c95ff952fc61232d5523cfe654c5f4a
Raven K <85319728+ravenkmusic@users.noreply.github.com>
2022-01-08 19:40:22 +0000

    removed unnecessary else statements

--- main.py
+++ main.py
@@ -33,18 +33,22 @@ num_choices = len(choices)
 
 users_choice = choices[int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))]
 
-computer_choice = choices[random.randint(0, num_choices)]
+computer_choice = choices[random.randint(0, num_choices - 1)]
+
+
+print(f"You chose: {users_choice}")
+print(f"Computer chose: {computer_choice}")
 
 if users_choice == computer_choice:
-  print(f"You both chose {str(computer_choice)}. It's a draw.")
-if users_choice == 0 and computer_choice == 2:
+  print(f"You both chose the same thing. It's a draw.")
+elif users_choice == 0 and computer_choice == 2:
   print("You win.")
-else:
-    print(f"Computer chose {computer_choice}. You lose.")
-if users_choice == 1 and computer_choice == 0:
+
+elif users_choice == 1 and computer_choice == 0:
   print("You win.")
-else: ("you lose.")
-if users_choice == 2 and computer_choice == 1:
+
+elif users_choice == 2 and computer_choice == 1:
   print("You win.")
+
 else:
   print("You lose.")
